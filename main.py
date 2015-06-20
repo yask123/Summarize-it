29106e2ea58d231b127622faa40eb006e9dcb478
Ketan Bhatt <ketanbhatt1006@gmail.com>
2015-06-20 16:57:59 +0530

    Changed directory structure

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from config import *
 import requests
 import pycps
+from slacker import Slacker
+import json
+
+slack = Slacker('xoxp-6562741812-6562848885-6651067744-cb5f98')
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/summarize", methods=['POST'])
+def slack():
+	print ("Slash command")
+	return ("Vinayak good boy")
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+# response =  slack.channels.history('C06GJNJEM')
+
+# a = (response.body)
+
+# print (a['messages'][0]['text'])
+
+
 
 # payload = {'apikey': secret["HP"], 'text': 'Someone I know recently combined Maple Syrup.'}
 
