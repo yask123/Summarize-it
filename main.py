831a68368b5fabe794603e016f52745a43e4dd98
Pranu Sarna <pranusarna@gmail.com>
2015-06-20 17:26:52 +0530

    Resolved Port time error

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def slack():
 	return ("Vinayak good boy")
 
 if __name__ == "__main__":
-    app.run()
+	# Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
