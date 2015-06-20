bcd836b44a34db73068b0fcb56e84d3c411e0704
Ketan Bhatt <ketanbhatt1006@gmail.com>
2015-06-20 15:51:25 +0530

    Trying out Clusterpoint. Created requirements.txt

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 from config import *
 import requests
 
-payload = {'apikey': secret["HP"], 'text': 'Someone I know recently combined Maple Syrup.'}
+# payload = {'apikey': secret["HP"], 'text': 'Someone I know recently combined Maple Syrup.'}
 
-r = requests.get('http://api.idolondemand.com/1/api/sync/extractconcepts/v1', params=payload)
+# r = requests.get('http://api.idolondemand.com/1/api/sync/extractconcepts/v1', params=payload)
 
-print r.text
+# print r.text
+payload = {'some': 'data'}
+cp = requests.post('https://api-us.clusterpoint.com/100581/Angel-hack/.json', data=payload)
