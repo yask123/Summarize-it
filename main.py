377381503a9c6e77fd1984dab14d5cd4f3c26dd9
Ketan Bhatt <ketanbhatt1006@gmail.com>
2015-06-21 12:58:00 +0530

    Final Commit - Debug=False

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ def slackReq():
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
